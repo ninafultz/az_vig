19d7572e014a325ee94a2b0c15b88cda63b1bf49
nina fultz <46796811+ninafultz@users.noreply.github.com>
2022-06-07 11:34:58 +0200

    Update gtm_seg.sh

--- pet_analysis/gtm_seg.sh
+++ pet_analysis/gtm_seg.sh
@@ -1,1 +1,1 @@
-for SUBJECT in az136 az124 az127 az131 az128 az129 az137 az141 az138 az149 az142 az154; do export SUBJECTS_DIR=/users/ninafultz/az_vig/${SUBJECT};  gtmseg --s fs/ --xcerseg;  done 
+for SUBJECT in $SUBJECT_LISR; do export SUBJECTS_DIR=$PROJ_DIR/${SUBJECT};  gtmseg --s fs/ --xcerseg;  done 
